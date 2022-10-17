7e0b2559a8f9e920cc778b8b8cce8e0686967670
Lafaiete Machado <lafaiete.machado@gmail.com>
2022-10-17 11:20:04 -0300

    Informações do dashboard do Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "kmodt8",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
